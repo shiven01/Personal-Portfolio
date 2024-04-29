003f72751eaf6186fef96d14ab6099cbeb7bed6f
Shiven Shekar <shivenshekar01@gmail.com>
2024-04-28 21:53:58 -0700

    new file:   contactInfo.html 	new file:   image1.jpg 	new file:   image2.jpg 	new file:   image3.jpg 	modified:   index.html 	new file:   slider.js 	new file:   style.css 	new file:   theBoys.html

---
+++ slider.js
@@ -0,0 +1,37 @@
+const slider = document.querySelector('.slider');
+const images = slider.querySelectorAll('img');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const imageDescription = document.querySelector('.image-description');
+
+let currentIndex = 0;
+const slideInterval = setInterval(nextSlide, 3000);
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % images.length;
+  slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+  updateImageDescription();
+}
+
+function prevSlide() {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+  updateImageDescription();
+}
+
+function updateImageDescription() {
+  const altText = images[currentIndex].getAttribute('alt');
+  imageDescription.textContent = altText;
+}
+
+prevBtn.addEventListener('click', () => {
+  clearInterval(slideInterval);
+  prevSlide();
+});
+
+nextBtn.addEventListener('click', () => {
+  clearInterval(slideInterval);
+  nextSlide();
+});
+
+updateImageDescription();
